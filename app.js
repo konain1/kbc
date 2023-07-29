02aaa840a7e3133644b8cf2ad57047b75bfc6b99
konain1 <kaunainkasab@gmail.com>
2023-07-29 16:23:06 +0530

    questionjs

---
+++ app.js
@@ -0,0 +1,59 @@
+
+
+
+const questions = [
+    {
+        sawal : 'who was the first Hokage',
+        jawab : [
+            {text:'Tobirama', correct : false},
+            {text:'Hashirama', correct : true},
+            {text:'Naruto', correct : false},
+            {text:'Kakasi', correct : false}
+        ]
+    },
+    {
+        sawal : 'who Kill the all uchiha clain',
+        jawab : [
+            {text:'itachi', correct : true},
+            {text:'obitu', correct : false},
+            {text:'sauske', correct : false},
+            {text:'Kakasi', correct : false}
+        ]
+    },
+    {
+        sawal : 'which was not a jiraya student',
+        jawab : [
+            {text:'Nagato', correct : false},
+            {text:'Minato', correct : false},
+            {text:'Naruto', correct : false},
+            {text:'itachi', correct : true}
+        ]
+    }
+  
+]
+
+
+const questionElement = document.getElementById('question-area');
+
+const answerDiv = document.getElementById('answerButton')
+const Next = document.getElementById('nextButton')
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+function startKBC(){
+    currentQuestionIndex = 0;
+    score = 0;
+    Next.innerHTML = 'Next'
+    showQuestion();
+}
+
+function showQuestion(){
+
+    let currentQuestion = questions[currentQuestionIndex]
+    let questionNo = currentQuestionIndex + 1
+    console.log(currentQuestion)
+}
+showQuestion()
